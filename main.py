7c8229024acc1cc0d0f78b5f9ce9943ae062ef66
Evan Dartez <98479231+esdartez@users.noreply.github.com>
2022-05-11 21:54:45 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,22 @@ import random, time
 import tabulate
 
 
+def select_random(a):
+    return random.choice(a)
+
+def select_first(a):
+    return a[0]
+
+
 def qsort(a, pivot_fn):
-    ## TO DO
-    pass
+    if len(a) <= 1:
+        return a
+    else:
+        p = pivot_fn(a)
+        l = list(filter(lambda x: x < p, a))
+        r = list(filter(lambda x: x > p, a))
+        c = list(filter(lambda x: x==p, a))
+        return qsort(l, pivot_fn) + c + qsort(r, pivot_fn)
     
 def time_search(sort_fn, mylist):
     """
@@ -40,19 +53,19 @@ def compare_sort(sizes=[100, 200, 500, 1000, 2000, 5000, 10000, 20000, 50000, 10
       for each method to run on each value of n
     """
     ### TODO - sorting algorithms for comparison
-    qsort_fixed_pivot = # 
-    qsort_random_pivot = #
-    tim_sort = #
+    quicksort_fixed_pivot = lambda a: qsort(a, select_first)
+    quicksort_random_pivot = lambda a: qsort(a, select_random)
+    tim_sort = sorted
     result = []
     for size in sizes:
-        # create list in ascending order
-        mylist = list(range(size))
-        # shuffles list if needed
-        #random.shuffle(mylist)
+        mylist = list(range(int(size)))
+        random.shuffle(mylist)
         result.append([
             len(mylist),
-            time_search(qsort_fixed_pivot, mylist),
-            time_search(qsort_random_pivot, mylist),
+            #time_search(selection_sort, mylist),
+            time_search(quicksort_fixed_pivot, mylist),
+            time_search(quicksort_random_pivot, mylist),
+            time_search(tim_sort, mylist)
         ])
     return result
     ###
